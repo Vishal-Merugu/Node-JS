d6945867b8cf9b5f04dfebf149d61bde2ed71601
Vishal-Merugu <vishalvish360@gmail.com>
2023-03-11 22:35:39 +0530

    with url node + about + home

--- First/first-server.js
+++ First/first-server.js
@@ -1,6 +1,36 @@
 const http = require('http');
 const server = http.createServer((request,response) =>{
-    console.log("Vishal Babu");
+    const url = request.url
+    if(url.includes('/home')){
+        response.setHeader('Context-Type', 'text/html')
+        response.write('<html>')
+        response.write("<head><title>HOME</title></head>");
+        response.write('<body>')
+        response.write('<h1>Welcome HOME</h1>')
+        response.write('</body>');
+        response.write('</html>');
+        response.end();
+    } else if (url.includes('/about')){
+        response.setHeader('Context-Type', 'text/html')
+        response.write('<html>')
+        response.write("<head><title>About US</title></head>");
+        response.write('<body>')
+        response.write('<h1>Welcome to About us page</h1>')
+        response.write('</body>');
+        response.write('</html>');
+        response.end();
+    } else if(url.includes('/node')){
+        response.setHeader('Context-Type', 'text/html')
+        response.write('<html>')
+        response.write("<head><title>Node</title></head>");
+        response.write('<body>')
+        response.write('<h1>Welcome to my Node JS Project</h1>')
+        response.write('</body>');
+        response.write('</html>');
+        response.end();
+    }
+    
 });
 
 server.listen(4000)
+
