684ea34d5238c83714ebffa2d8c7266cf6b23c44
Vishal-Merugu <vishalvish360@gmail.com>
2023-03-12 19:37:23 +0530

    form with stream and buffers to msg.txt

--- First/first-server.js
+++ First/first-server.js
@@ -1,36 +1,43 @@
 const http = require('http');
-const server = http.createServer((request,response) =>{
-    const url = request.url
-    if(url.includes('/home')){
-        response.setHeader('Context-Type', 'text/html')
-        response.write('<html>')
-        response.write("<head><title>HOME</title></head>");
-        response.write('<body>')
-        response.write('<h1>Welcome HOME</h1>')
-        response.write('</body>');
-        response.write('</html>');
-        response.end();
-    } else if (url.includes('/about')){
-        response.setHeader('Context-Type', 'text/html')
-        response.write('<html>')
-        response.write("<head><title>About US</title></head>");
-        response.write('<body>')
-        response.write('<h1>Welcome to About us page</h1>')
-        response.write('</body>');
-        response.write('</html>');
-        response.end();
-    } else if(url.includes('/node')){
-        response.setHeader('Context-Type', 'text/html')
-        response.write('<html>')
-        response.write("<head><title>Node</title></head>");
-        response.write('<body>')
-        response.write('<h1>Welcome to my Node JS Project</h1>')
-        response.write('</body>');
-        response.write('</html>');
-        response.end();
-    }
-    
-});
+const fs = require('fs');
 
-server.listen(4000)
+const server = http.createServer((request,response) =>{
+    const url = request.url;
+    const method = request.method;
+    if(url === '/'){
+        response.setHeader('Context-Type', 'text/html')
+        response.write('<html>')
+        response.write("<head><title>Enter Message</title></head>");
+        response.write('<body>')
+        response.write('<form action = "/message" method = "POST"><input type = "text" name = "message" ><button type = "submit">Send</button></form>')
+        response.write('</body>');
+        response.write('</html>');
+        return response.end();
+    } 
+    if(url === '/message' && method === "POST"){
+        const body = []
+        request.on('data',(chunk) =>{
+            console.log(chunk);
+            body.push(chunk)
+        })
+        request.on('end',() =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            console.log(message);
+            fs.writeFileSync('message.txt', message);
+        })
+        response.statusCode = 302;
+        response.setHeader('Location','/');
+        return response.end();
+    }
+    // response.setHeader('Content-Type','text/html');
+    // response.write("<html>");
+    // response.write("<head><title>Hello from Node JS</title></head>");
+    // response.write("<body>Hello</body>")
+    // response.write("</html>");
+    // response.end();
+
+});
+server.listen(3000)
+
 
